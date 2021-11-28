371cc6461871616f5516b92cb8c08c86f4005305
moritzdanhausen <morit.danhausen@gmx.de>
2021-11-28 20:40:51 +0100

    initialize

---
+++ Olympia2/src/compositePattern/OlympicTeams.java
@@ -0,0 +1,21 @@
+package compositePattern;
+
+public abstract class OlympicTeams {
+	
+	public static void add() {
+		
+	};
+	
+	public static String getTeamName() {
+		return "";
+	}
+	
+	public static int getNumberOfGoldMedals() {
+		return 0;
+	}
+	
+	public static int getNumberOfAtlethes() {
+		return 0;
+	}
+
+}
